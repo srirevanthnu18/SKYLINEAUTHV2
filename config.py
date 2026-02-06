da03804953fa795de3c5d7041c5d90e53cd2e327
Adityashree12 <adityashree@example.com>
2026-02-06 13:28:18 +0530

    Secure credentials and add PythonAnywhere support

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'neutron-change-this-secret-key-in-production')
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://Adityashree12:srishtisingh0725@cluster0.82cjuzd.mongodb.net/?appName=Cluster0')  # Set MONGO_URI env var on Render
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MONGO_URI = os.environ.get('MONGO_URI')
     DATABASE_NAME = os.environ.get('DATABASE_NAME', 'neutron')
     BACKUP_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backups')
